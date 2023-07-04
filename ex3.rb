1b6cedc8a981860ca3d42bda7bd19cff72441caf
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-04 19:00:52 +0300

    Working with Strings and Text

---
+++ ex3.rb
@@ -0,0 +1,21 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said #{x}"
+puts "I also said #{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = 'This ia left side of a string with ... '
+e = 'right hand side'
+
+puts w + e
