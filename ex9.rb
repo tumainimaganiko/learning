8b06a2361421d09659f475ba8af3958aeda4ed9c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-05 09:08:43 +0300

    More Printing

---
+++ ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+month = "Jan\nFeb\nMarch\nApril\nMay\nJune\nJuly\nAug"
+
+puts "These are the days #{days}"
+puts "These are the months #{month}"
+
+puts %q{
+    There is something going on here.
+    With this weird quote
+    We'll be able to type as much as we like
+    Even 4 lines if we want, or 5, or 6
+}
