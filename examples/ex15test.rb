929de0ee845c2c3539b50d5147aec057aaa52a5d
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-06 17:28:53 +0300

    Reading & Writing Files

--- examples/ex15test.rb
+++ examples/ex15test.rb
@@ -3,7 +3,7 @@ filename = ARGV.first
 puts "We're going to erase #{filename}"
 puts "If you don't want that hit CTRL+C (^C)."
 puts "If you don't want that, hit RETURN."
-# Receiving user choice whether to proceed or cancel
+
 $stdin.gets
 
 puts "Opening the file ...."
@@ -14,21 +14,10 @@ target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
-print "line1: "
-line1 = $stdin.gets.chomp
-print "line2: "
-line2 = $stdin.gets.chomp
-print "line3: "
-line3 = $stdin.gets.chomp
+formatter = "%{line1} %{line2} %{line3}"
 
-puts "I am going to write these to the file."
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
+target.write(
+    formatter % {line1: $stdin.gets.chomp, line2: $stdin.gets.chomp,line3: $stdin.gets.chomp}
+)
 puts "And finally we close it "
 target.close
